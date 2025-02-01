01de440e9254e2852f025cda10f160fe35b19799
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2025-02-01 13:07:32 -0300

    Enviando atualização da rota feita no app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { HashRouter , Route, Routes,Navigate  } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout/Layoutfile/Layout";
 import { Contato } from "./page/Contato/Contato";
 import { Certificado } from "./page/Certificado/Certificado";
-import { HomePage} from './page/Home/HomePage';
+import { HomePage } from './page/Home/HomePage';
 import { SigningPage } from "./page/SignIn/SigningPage";
 import "./App.css";
 import { PerfilPage } from "./page/Perfil/PerfilPage";
@@ -14,23 +14,20 @@ import { ProjetoPage } from "./page/ProjetoPage/ProjetoPage";
 function App() {
   return (
     <HashRouter>
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projetos" element={<ProjetoPage />} />
-        <Route path="/certificado" element={<Certificado />} />
-        <Route path="/contate-me" element={<Contato />} />
-        <Route path="/entrar" element={<SigningPage />} />
-        <Route path="/perfil" element={<PerfilPage />} />
-        <Route path="/editar/:id" element={<Editar />} />
-        <Route path="/visualizar/:id" element={<Visualizar />} />
-        <Route path="/meu-post/:id" element={<MeusPosts />} />
-        <Route path="/home" element={<Navigate to="/" />} />
-      </Routes>
-    </Layout>
-  </HashRouter>
-  
-
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projetos" element={<ProjetoPage />} />
+          <Route path="/certificado" element={<Certificado />} />
+          <Route path="/contate-me" element={<Contato />} />
+          <Route path="/entrar" element={<SigningPage />} />
+          <Route path="/perfil" element={<PerfilPage />} />
+          <Route path="/editar/:id" element={<Editar />} />
+          <Route path="/visualizar/:id" element={<Visualizar />} />
+          <Route path="/meu-post/:id" element={<MeusPosts />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
   );
 }
 
