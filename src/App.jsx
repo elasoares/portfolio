156b77f614e3734ce29864f300b19ec5a75180e6
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2025-02-01 13:00:19 -0300

    Enviando atualização da rota feita no arquivo app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter , Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter , Route, Routes,Navigate  } from "react-router-dom";
 import { Layout } from "./Layout/Layoutfile/Layout";
 import { Contato } from "./page/Contato/Contato";
 import { Certificado } from "./page/Certificado/Certificado";
@@ -14,21 +14,22 @@ import { ProjetoPage } from "./page/ProjetoPage/ProjetoPage";
 function App() {
   return (
     <HashRouter>
-      <Layout>
-        <Routes>
-        <Route path="/" element={<Navigate to="/#home" />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/projetos" element={<ProjetoPage />} />
-          <Route path="/certificado" element={<Certificado />} />
-          <Route path="/contate-me" element={<Contato />} />
-          <Route path="/entrar" element={<SigningPage />} />
-          <Route path="/perfil" element={<PerfilPage />} />
-          <Route path="/editar/:id" element={<Editar />} />
-          <Route path="/visualizar/:id" element={<Visualizar />} />
-          <Route path="/meu-post/:id" element={<MeusPosts />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" />}/>
+        <Route path="/projetos" element={<ProjetoPage />} />
+        <Route path="/certificado" element={<Certificado />} />
+        <Route path="/contate-me" element={<Contato />} />
+        <Route path="/entrar" element={<SigningPage />} />
+        <Route path="/perfil" element={<PerfilPage />} />
+        <Route path="/editar/:id" element={<Editar />} />
+        <Route path="/visualizar/:id" element={<Visualizar />} />
+        <Route path="/meu-post/:id" element={<MeusPosts />} />
+      </Routes>
+    </Layout>
+  </HashRouter>
+  
 
   );
 }
