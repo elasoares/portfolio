7022e0ae134b4bf678b8735e4651ced2169c2ed8
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2025-05-09 23:32:52 -0300

    Enviando atualização do css do arquivo Layout

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,7 @@
-
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase} from 'firebase/database'; 
-import { getStorage } from 'firebase/storage';
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyACG4Lbv2LV34Ui3sTErBapEZtxtRsj7GU",
@@ -15,12 +13,11 @@ const firebaseConfig = {
   appId: "1:50618352741:web:be70b11adec653a873f28c",
   measurementId: "G-05ZED8B9LV"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app); 
- const auth = getAuth( app);
- const dataBase = getDatabase(app);
- const storage = getStorage(app);
+// Initialize Firebase
+const auth = getAuth(app);
+const dataBase = getDatabase(app);
+const storage = getStorage(app);
 
-export{analytics, auth, dataBase, storage};
+
+ export { auth, dataBase, storage };
